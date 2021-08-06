f53c5087f93d473184d3246005c0a548657d2a6b
aedelgado19 <aedelgado19@gmail.com>
2021-08-05 17:31:35 -0700

    .h

---
+++ Olympics.h
@@ -0,0 +1,54 @@
+/* Author: Allison Delgado
+   Class: CS 202 Summer 2021
+   Olympics.h contains the function prototypes
+   for the following classes: Sport, Athlete,
+   Track (track and field), Tennis, VB (volleyball),
+   BST, B_Node, LLL, and L_Node.
+   The athletes will be storesd in a BST.
+
+   Last updated: Aug 5, 2021
+ */
+
+#include <iostream>
+#include <cstring>
+
+class Sport {
+
+};
+
+class Athlete {
+
+};
+
+class Track : public Sport {
+
+
+};
+
+class Tennis : public Sport {
+
+
+};
+
+class VB : public Sport {
+
+};
+
+// ***************** DATA STRUCTURE CLASSES **************
+
+class BST {
+
+};
+
+class B_Node {
+
+};
+
+class L_Node {
+
+
+};
+
+class LLL {
+
+};
